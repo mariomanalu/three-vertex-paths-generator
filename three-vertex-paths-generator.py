b9331b4cd4fc22d1dbcd2f4326f3535768df6e90
mariomanalu <1999manalu@gmail.com>
2020-08-28 12:48:53 -0700

    Fixed bugs

--- three-vertex-paths-generator.py
+++ three-vertex-paths-generator.py
@@ -1,36 +1,25 @@
-print("Welcome to the complete bipartite path generator program!")
-def ask():
-    answer = input("Would you like to know the definition of complete bipartite graph? [y/n]")
+import sys
 
-    if answer == "y":
-        print("A complete bipartite graph is a bipartite graph where every vertex of the first set is adjacent to every vertex of the second set")
-    elif answer == "n":
-        pass
-    else:
-        print("Unknown command. Please enter y or n")
-        ask()
+stdoutOrigin=sys.stdout
+sys.stdout = open("example.txt", "w")
 
-ask()
+size_of_first_set = int(input("Enter the number of vertices in the first subset: "))
+size_of_second_set = int(input("Enter the number of vertices in the second subset: "))
 
-def enter_size_of_sets_and_verify():
+while size_of_first_set + size_of_second_set < 3:
+    print("There is no path of length three vertices in a graph of vertices less than 3")
     size_of_first_set = int(input("Enter the number of vertices in the first subset: "))
     size_of_second_set = int(input("Enter the number of vertices in the second subset: "))
-
-    if size_of_first_set + size_of_second_set < 3:
-        print("There is no path of length three vertices in a graph of vertices less than 3")
-        enter_size_of_sets_and_verify()
-    else:
-        pass
-
-enter_size_of_sets_and_verify()
 
 first_set = []
 second_set = []
 set_of_three_vertex_paths = []
 
+'''Initializing the first set '''
 for i in range(size_of_first_set):
     first_set.append("V" + str(i + 1))
 
+'''Initializing the second set '''
 for i in range(size_of_second_set):
     second_set.append("W" + str(i + 1))
 
@@ -48,5 +37,13 @@ for first_vertex in range(size_of_second_set):
         for third_vertex in range(first_vertex+1, size_of_second_set):
             set_of_three_vertex_paths.append(second_set[first_vertex] + "-" + first_set[second_vertex] + "-" + second_set[third_vertex])
 
-print(set_of_three_vertex_paths)
-print(len(set_of_three_vertex_paths))
+
+'''Printing the paths '''
+for path in set_of_three_vertex_paths:
+    print(path)
+
+'''Printing the number of paths'''
+if len(set_of_three_vertex_paths) == 1:
+    print("There is 1 path of length 3 vertex")
+else:
+    print("There are", len(set_of_three_vertex_paths), "paths of length 3 vertex")
